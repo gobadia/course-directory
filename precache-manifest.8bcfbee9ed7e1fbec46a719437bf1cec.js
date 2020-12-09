06e13693e15127121eb652965cea801835fff222
gobadia <gabrielobadia@gmail.com>
2020-12-08 19:26:04 -0500

    Updates

---
+++ precache-manifest.8bcfbee9ed7e1fbec46a719437bf1cec.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "3cc15cc1942201d21f45",
+    "url": "/course-directory/static/css/main.a805af84.chunk.css"
+  },
+  {
+    "revision": "3cc15cc1942201d21f45",
+    "url": "/course-directory/static/js/main.3cc15cc1.chunk.js"
+  },
+  {
+    "revision": "f1114e3871cf27b0485b",
+    "url": "/course-directory/static/js/1.f1114e38.chunk.js"
+  },
+  {
+    "revision": "527751acb6022af06d5d",
+    "url": "/course-directory/static/js/runtime~main.527751ac.js"
+  },
+  {
+    "revision": "268387c4abfdcb638b23ad05bb8e2dc3",
+    "url": "/course-directory/index.html"
+  }
+];
